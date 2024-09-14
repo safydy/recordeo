220b99a7ecafe6cc105e8d092f70d7124efec85b
safydy <r.safydy@gmail.com>
2024-09-13 18:01:30 -0600

    f1/3 do not send non-serializable data to redux

--- components/Recorder.tsx
+++ components/Recorder.tsx
@@ -1,61 +1,118 @@
 'use client';
-import { useSelector, useDispatch } from 'react-redux';
 import RecordRTC from 'recordrtc';
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import Box from "@mui/material/Box";
 import RecordControlButtons from "./RecordControlButtons";
 import ConfigPanel from "@/components/ConfigPanel";
 import {
     getScreenDimension,
-    getCanvasRef,
-    getRecordedBlob,
-    getCombinedStream,
-    stopRecording,
-    setScreenStream,
-    setWebcamStream,
-    combineStreams,
-    updateRecorderRef,
+    getWebcamDimension,
+    getWebcamPosition,
+    getCanvasDimension,
     updateStatus,
-    canvasUpdateRef,
 } from "@/stores/features/recorder";
 import { useAppSelector, useAppDispatch } from "@/utils/storeHooks";
+import {circleClip} from "@/utils/shapeDrawer";
 
 interface RecorderProps {
     // Potential props for customization
 }
 
 const Recorder: React.FC<RecorderProps> = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const canvasDimension = useAppSelector(getCanvasDimension);
+
     const screenDimension = useAppSelector(getScreenDimension);
-    const combinedStream = useAppSelector(getCombinedStream);
-    const recordedBlob = useAppSelector(getRecordedBlob);
+
+    const webcamDimension = useAppSelector(getWebcamDimension);
+    const webcamPosition = useAppSelector(getWebcamPosition);
+
+    const recorderRef = useRef<MediaRecorder | null>(null);
+    // Pay attention, useState is async, so you can't rely on the value immediately after setting it
+    const [screenStream, setScreenStream] = useState<MediaStream | null>(null);
+    const [webcamStream, setWebcamStream] = useState<MediaStream | null>(null);
+    const [combinedStream, setCombinedStream] = useState<MediaStream | null>(null);
+    const [recordedBlob, setRecordedBlob] = useState<Blob | null>(null);
+
     const dispatch = useAppDispatch();
 
-    useEffect(() => {
-        if (canvasRef.current) {
-            dispatch(canvasUpdateRef(canvasRef.current));
-        }
-    }, [canvasRef]);
-
     const handleStartRecording = async () => {
-        await dispatch(setScreenStream());
-        await dispatch(setWebcamStream());
-        await dispatch(combineStreams());
+        try {
+            const currentScreenStream = await getNewScreenStream() as MediaStream;
+            const currentWebcamStream = await getNewWebcamStream() as MediaStream;
+            setScreenStream(currentScreenStream);
+            setWebcamStream(currentWebcamStream);
 
-        if (!combinedStream) {
-            console.error('no combined stream found');
+            const currentCombinedStream = getCombinedStreams(currentScreenStream, currentWebcamStream);
+            setCombinedStream(currentCombinedStream);
+
+            if (!currentCombinedStream) {
+                console.error('no combined stream found');
+                return;
+            }
+
+            const recorder = RecordRTC(currentCombinedStream, {type: 'video'});
+            recorderRef.current = recorder;
+            recorder.startRecording();
+
+            dispatch(updateStatus("recording"));
+        } catch (error) {
+            console.error('Error starting recording:', error);
+        }
+    }
+
+    /**
+     * Combines the screen and webcam streams into a single stream
+     * Refs cannot be put in Redux, so we have to declare this in the component instead
+     */
+    const getCombinedStreams = (currentScreenStream: MediaStream, currentWebcamStream: MediaStream) => {
+        const canvas = canvasRef.current;
+        if (!canvas) {
+            console.error('Canvas ref not defined');
             return;
         }
 
-        const recorder = RecordRTC(combinedStream, { type: 'video' });
-        dispatch(updateRecorderRef(recorder))
-        recorder.startRecording();
+        const screenVideo = document.createElement('video');
+        screenVideo.srcObject = currentScreenStream as MediaStream;
+        screenVideo.play();
 
-        dispatch(updateStatus("recording"));
+        const webcamVideo = document.createElement('video');
+        webcamVideo.srcObject = currentWebcamStream as MediaStream;
+        webcamVideo.play();
+
+        const context = canvas.getContext('2d');
+        if (!context) return;
+
+        const draw = () => {
+            context.drawImage(screenVideo, 0, 0, canvasDimension.width, canvasDimension.height);
+            const avatarRadius = webcamDimension.width / 2; // Radius of the circle
+            //TODO: get x and y from webcamPosition
+            const x = 50; //webcamPosition.x;
+            const y = 50; //webcamPosition.y;
+            circleClip(context, x, y, avatarRadius, () => {
+                context.drawImage(webcamVideo, x, y, webcamDimension.width, webcamDimension.height);
+            })
+
+            requestAnimationFrame(draw);
+        };
+
+        draw();
+        return canvas.captureStream();
     }
 
-    const handleStopRecording = () => {
-        dispatch(stopRecording({shouldSetBlob: true}))
+    const handleStopRecording = (action: {shouldSetBlob: boolean} = {shouldSetBlob: true}) => {
+        if (recorderRef.current) {
+            recorderRef.current.stopRecording(() => {
+                screenStream?.getTracks().forEach(track => track.stop());
+                webcamStream?.getTracks().forEach(track => track.stop());
+                combinedStream?.getTracks().forEach(track => track.stop());
+
+                if (action.shouldSetBlob) {
+                    setRecordedBlob(recorderRef.current?.getBlob());
+                }
+                dispatch(updateStatus("stopped"));
+            });
+        }
     }
 
     return (
@@ -76,3 +133,30 @@ const Recorder: React.FC<RecorderProps> = () => {
 };
 
 export default Recorder;
+
+export const getNewScreenStream = async () => {
+    try {
+        const displayMediaOptions: MediaStreamConstraints = {
+            video: true,
+        };
+
+        // For selecting a specific window, you can use the following:
+        // displayMediaOptions.video = { mediaSource: 'window' };
+
+        const stream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+        return stream;
+    } catch (error) {
+        console.error('Error capturing screen:', error);
+        return null;
+    }
+}
+
+export const getNewWebcamStream = async () => {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        return stream;
+    } catch (error) {
+        console.error('Error capturing webcam:', error);
+        return null;
+    }
+}
--- stores/features/recorder.tsx
+++ stores/features/recorder.tsx
@@ -13,50 +13,38 @@ export type RecordingStatusType = "initialized" | "recording" | "paused" | "stop
 
 export interface RecorderState {
     status: RecordingStatusType
-    recorderRef: MediaRecorder | null,
-    recordedBlob: Blob | null,
-    combinedStream: MediaStream | null,
     webcam: {
         dimension: IDimension
         position: CategoricalPositionType,
-        stream: MediaStream | null,
     },
     screen: {
         dimension: IDimension,
-        stream: MediaStream | null,
     },
     canvas: {
         dimension: IDimension,
-        ref: HTMLVideoElement | null,
     }
 }
 
 const initialState: RecorderState = {
     status: "initialized",
-    recorderRef: null,
-    recordedBlob: null,
-    combinedStream: null,
     webcam: {
         dimension: {
             width: 64,
             height: 64,
         },
         position: "top-left",
-        stream: null,
     },
     screen: {
         dimension: {
             width: 1024,
             height: 768,
         },
-        stream: null,
     },
     canvas: {
         dimension: {
             width: 1024,
             height: 768,
         },
-        ref: null,
     }
 }
 
@@ -67,91 +55,26 @@ export const recorderSlice = createSlice({
         updateStatus: (state, action: PayloadAction<RecordingStatusType>) => {
             state.status = action.payload
         },
-        updateRecorderRef: (state, action: PayloadAction<MediaRecorder | null>) => {
-            state.recorderRef = action.payload
-        },
         webcamUpdateDimensions: (state, action: PayloadAction<IDimension>) => {
             state.webcam.dimension = action.payload
         },
         webcamUpdatePosition: (state, action: PayloadAction<CategoricalPositionType>) => {
             state.webcam.position = action.payload
         },
-        webcamUpdateStream: (state, action: PayloadAction<MediaStream | null>) => {
-            state.webcam.stream = action.payload
-        },
         screenUpdateDimensions: (state, action: PayloadAction<IDimension>) => {
             state.screen.dimension = action.payload
         },
-        screenUpdateStream: (state, action: PayloadAction<MediaStream | null>) => {
-            state.screen.stream = action.payload
-        },
-        canvasUpdateRef: (state, action: PayloadAction<HTMLCanvasElement | null>) => {
-            state.canvas.ref = action.payload
-        },
         canvasUpdateDimensions: (state, action: PayloadAction<IDimension>) => {
             state.canvas.dimension = action.payload
-        },
-        combineStreams: (state) => {
-            const canvas = state.canvas.ref;
-            if (!canvas) {
-                console.error('Canvas ref not defined');
-                return;
-            }
-
-            const screenVideo = document.createElement('video');
-            screenVideo.srcObject = state.screen.stream;
-            screenVideo.play();
-
-            const webcamVideo = document.createElement('video');
-            webcamVideo.srcObject = state.webcam.stream;
-            webcamVideo.play();
-
-            const context = canvas.getContext('2d');
-            if (!context) return;
-
-            const draw = () => {
-                context.drawImage(screenVideo, 0, 0, state.canvas.dimension.width, state.canvas.dimension.height);
-                const avatarRadius = state.webcam.dimension.width / 2; // Radius of the circle
-                const x = state.webcam.position.x;
-                const y = state.webcam.position.y;
-                circleClip(context, x, y, avatarRadius, () => {
-                    context.drawImage(webcamVideo, x, y, state.webcam.dimension.width, state.webcam.dimension.height);
-                })
-
-                requestAnimationFrame(draw);
-            };
-
-            draw();
-            state.combinedStream = canvas.captureStream();
-        },
-        stopRecording: (state, action: PayloadAction<{shouldSetBlob: boolean}>) => {
-            if (state.recorderRef) {
-                state.recorderRef.stopRecording(() => {
-                    state.screen.stream?.getTracks().forEach(track => track.stop());
-                    state.webcam.stream?.getTracks().forEach(track => track.stop());
-                    state.combinedStream?.getTracks().forEach(track => track.stop());
-
-                    if (action.payload.shouldSetBlob) {
-                        state.recordedBlob = state.recorderRef?.getBlob();
-                    }
-                    state.status = "stopped";
-                });
-            }
         },
     },
 })
 
 export const {
     updateStatus,
-    updateRecorderRef,
     webcamUpdateDimensions,
     webcamUpdatePosition,
-    webcamUpdateStream,
     screenUpdateDimensions,
-    screenUpdateStream,
-    combineStreams,
-    stopRecording,
-    canvasUpdateRef,
     canvasUpdateDimensions,
 } = recorderSlice.actions
 
@@ -167,10 +90,7 @@ export const getStatus = (state: RootState) => state.recorder.status
 export const getWebcamDimension = (state: RootState) => state.recorder.webcam.dimension
 export const getWebcamPosition = (state: RootState) => state.recorder.webcam.position
 export const getScreenDimension = (state: RootState) => state.recorder.screen.dimension
-export const getCanvasRef = (state: RootState) => state.recorder.canvas.ref
-export const getCombinedStream = (state: RootState) => state.recorder.combinedStream
-export const getRecordedBlob = (state: RootState) => state.recorder.recordedBlob
-
+export const getCanvasDimension = (state: RootState) => state.recorder.canvas.dimension
 /**
  * ######## Thunks #######
  * which can contain both sync and async logic
@@ -178,29 +98,3 @@ export const getRecordedBlob = (state: RootState) => state.recorder.recordedBlob
  * Eg: check here https://codesandbox.io/p/sandbox/github/reduxjs/redux-templates/tree/master/packages/rtk-app-structure-example?file=%2Fsrc%2Ffeatures%2Fcounter%2FcounterSlice.ts%3A81%2C27&from-embed=
  * #######################
  */
-export const setScreenStream = (): AppThunk => async (dispatch, getState) => {
-    try {
-        const displayMediaOptions: MediaStreamConstraints = {
-            video: true,
-        };
-
-        // For selecting a specific window, you can use the following:
-        // displayMediaOptions.video = { mediaSource: 'window' };
-
-        const stream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
-        dispatch(screenUpdateStream(stream));
-    } catch (error) {
-        console.error('Error capturing screen:', error);
-        dispatch(screenUpdateStream(null));
-    }
-}
-
-export const setWebcamStream = (): AppThunk => async (dispatch, getState) => {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-        dispatch(webcamUpdateStream(stream));
-    } catch (error) {
-        console.error('Error capturing webcam:', error);
-        dispatch(webcamUpdateStream(null));
-    }
-}
